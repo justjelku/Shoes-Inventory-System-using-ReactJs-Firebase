f17c3a30a59531b68c9d362e6fb226d2f7d825b6
Jeremiah Larry King Ungsod <67980301+Jeremiah019@users.noreply.github.com>
2023-05-04 14:02:34 +0800

    Added ProductForm

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 import Navbar from './NavBar';
 import ContainerTables from './Tables';
 import DashboardContainers from './Dashboard';
+import Main from './Main';
+import AddProduct from './Addproduct';
 
 const Home
   = () => {
     return (
       <div>
-        <Navbar />
-        <DashboardContainers />
-        <ContainerTables />
+        <Navbar>
+          <AddProduct />
+        </Navbar>
+        <Main>
+          <DashboardContainers />
+          <ContainerTables />
+        </Main>
       </div>
     );
   }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,50 @@
 import React, {useContext, useState} from 'react'
-import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import { ProductContext } from '../context/ProductContextProvider';
 
 function Product({product}){
+	// STATE
+    const [hover, setHover] = useState(false)
+
+	// CONTEXT
+    const { selectedProduct, setSelectedProduct} = useContext(ProductContext)
+
+	const handleDelete = () => {
+		deleteProduct(product.id);
+	  
+		if (selectedProduct === product) {
+		  setSelectedProduct(undefined);
+		}
+	  }	  
+
+	const deleteProduct = (productId) => {
+		firebase
+		  .firestore()
+		  .collection('users')
+		  .doc('qIglLalZbFgIOnO0r3Zu')
+		  .collection('basic_users')
+		  .doc(userId)
+		  .collection('products')
+		  .doc(productId)
+		  .delete()
+		  .then(() => {
+			console.log(`Product with ID ${productId} successfully deleted`);
+		  })
+		  .catch((error) => {
+			console.error(`Error deleting product with ID ${productId}: ${error}`);
+		  });
+	  }
+	  
+
 	return ( 
 		<div className='Product'>
+			<div
+				className='product-container'
+				onMouseEnter={() => setHover(true)}
+				onMouseLeave={() => setHover(false)}
+			>
+			</div>
 		</div>
 	 );
 }
